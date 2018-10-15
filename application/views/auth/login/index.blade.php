19df853aa3050c7d6967c55563729bbc99ca5d83
aurakanzaa <aurakanzaa@yahoo.co.id>
2018-10-15 11:31:14 +0700

    login

--- application/views/auth/login/index.blade.php
+++ application/views/auth/login/index.blade.php
@@ -3,7 +3,17 @@
 @section('content')
 
 <div class="container">
+    <form action="">
+        <input type="text" id="fname" name="username" placeholder="Username">
+        <input type="password" id="fname" name="password" placeholder="Password">
+        <select id="role" name="role">
+            <option value="australia">Customer</option>
+            <option value="canada">Canada</option>
+            <option value="usa">USA</option>
+        </select>
 
+        <input type="submit" value="Log In">
+    </form>
 </div>
 
 @endsection
